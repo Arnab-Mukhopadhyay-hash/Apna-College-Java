4d2bf1b8d7ce901a2964faddecba1959f8ad2c41
Codie <arnabmukhopadhyay76@gmail.com>
2022-09-08 00:29:56 +0530

    Programs of Day 3

---
+++ OddEven.java
@@ -0,0 +1,15 @@
+package ApnaCollege;
+
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        if (n % 2 == 0)
+            System.out.println("Even Number");
+        else
+            System.out.println("Odd Number");
+        scan.close();
+    }
+}
