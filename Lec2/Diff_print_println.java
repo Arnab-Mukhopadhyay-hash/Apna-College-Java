3ca97a3fd6959cfa8921488376aeca6a7e36118f
Codie <arnabmukhopadhyay76@gmail.com>
2022-12-04 12:40:52 +0530

    Progs of lecture 2

---
+++ Lec2/Diff_print_println.java
@@ -0,0 +1,11 @@
+package Lec2;
+
+public class Diff_print_println {
+    public static void main(String[] args) {
+        System.out.print("This line will be printed without new line character at end");
+        System.out.print(
+                "This line will be printed on the same line as the previous statement but this time it has a new line character at end \n");
+        System.out.println(
+                "This line is printed in a new line becuase of new line character before and the next line will be printed in a new line as println has the new line character automatically.");
+    }
+}
